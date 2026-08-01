ce893fd7a2d753e0200f8be5bfe49cc147631c40
mirkofr <46991287+mirkofr@users.noreply.github.com>
2026-08-01 19:51:20 +0900

    lint: remove unused GP namespace import

--- src/motorch/models/gp.py
+++ src/motorch/models/gp.py
@@ -3,7 +3,6 @@
 from abc import abstractmethod
 from typing import cast
 
-import gpytorch
 import torch
 from gpytorch.distributions import MultivariateNormal
 from gpytorch.kernels import RBFKernel, ScaleKernel
